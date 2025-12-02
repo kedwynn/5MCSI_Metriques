0cc9e31cb55de47b2d290abe61577c5e2e223d28
Kedwynn <103037056+kedwynn@users.noreply.github.com>
2025-12-02 11:49:35 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,6 +11,12 @@ app = Flask(__name__)
 def contact():
     return render_template("contact.html")
 
+@app.route('/extract-minutes/<date_string>')
+def extract_minutes(date_string):
+        date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+        minutes = date_object.minute
+        return jsonify({'minutes': minutes})
+
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
