047c56361546c2d30786f615921c1c0a1e5e7ebc
Kedwynn <103037056+kedwynn@users.noreply.github.com>
2025-12-02 10:58:01 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -16,6 +16,18 @@ def MaPremiereAPI():
 def mongraphique():
     return render_template("graphique.html")
 
+@app.route('/histogramme/')
+def meteo1():
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
+
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
